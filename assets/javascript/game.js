af880fc6767ea362eddcb6dc10ef7e583eddc8e9
Victor Dominguez <vd071090@gmail.com>
2019-10-05 14:57:06 -0400

    added variables to js

---
+++ assets/javascript/game.js
@@ -0,0 +1,6 @@
+let answer = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+let userGuess = "";
+let computerGuess = "";
+let correct = false;
+let maxGuess = 10;
+
