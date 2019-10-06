54aa604fd9c85a6f4f3a3e7b6f3f9dabd074c538
Victor Dominguez <vd071090@gmail.com>
2019-10-05 22:26:10 -0400

    added style/reset css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,4 +3,58 @@ let userGuess = "";
 let computerGuess = "";
 let correct = false;
 let maxGuess = 10;
+let wins = 0;
+let losses = 0;
 
+
+
+
+
+    
+
+
+////////////////*************Start of game************************////////////////////////////////
+
+
+
+function setup() {
+    computerGuess = answer[Math.floor(Math.random()*answer.length)];
+    console.log(computerGuess);
+    (maxGuess = 10);
+
+
+
+
+
+
+        document.onkeyup = function(event) {
+            userGuess = event.key;
+            console.log(userGuess);
+            if ( userGuess === computerGuess){
+                wins++;
+                setup();      
+            }else{
+                maxGuess--;
+
+            }
+            if ( maxGuess === 0) {
+                losses++;
+                wrongGif();
+            
+
+            }
+            
+            document.getElementById("userchoice-text").innerHTML = "Your choice: " + userGuess;
+            document.getElementById("wins-text").innerHTML = "wins: " + wins;
+            document.getElementById("loss-text").innerHTML = "losses: " + losses;
+            document.getElementById("guesses-remaining").innerHTML = "Guesses remaining: " + maxGuess;
+           
+        }
+
+        }
+
+        function wrongGif() {
+            document.getElementById("wrong-gif").innerHTML = img=("https://media.giphy.com/media/TPl5N4Ci49ZQY/giphy.gif")
+        }
+
+setup();
